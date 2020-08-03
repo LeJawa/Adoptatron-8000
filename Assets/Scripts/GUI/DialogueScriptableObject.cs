e9b13ea2dee50d558b90ba652dce4271ae3d4e93
Luis Escobar Sawa <luis.devil77s@gmail.com>
2020-08-03 22:41:48 +0200

    Started working on text system

---
+++ Assets/Scripts/GUI/DialogueScriptableObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace SparuvianConnection.Adoptatron.GUI
+{
+    [CreateAssetMenu(menuName = "New dialogue")]
+    public class DialogueScriptableObject : ScriptableObject
+    {
+        
+        [TextArea]
+        public string[] dialogueLinesArray;
+
+        [NonSerialized]
+        private int _currentIndex = 0;
+
+        private void Awake()
+        {
+            _currentIndex = 0;
+        }
+
+        public string GetNextLine()
+        {
+            if (_currentIndex >= dialogueLinesArray.Length) return null;
+            
+            _currentIndex++;
+            return dialogueLinesArray[_currentIndex - 1];
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/GUI/DialogueTester.cs
@@ -0,0 +1,42 @@
+﻿using SparuvianConnection.Adoptatron.Utils;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SparuvianConnection.Adoptatron.GUI
+{
+    public class DialogueTester : MonoBehaviour
+    {
+        public Text text;
+        private TextWriter.TextWriterSingle textWriterSingle;
+        
+        private DialogueScriptableObject _dialogue;
+
+        private void Start()
+        {
+            _dialogue = Resources.Load<DialogueScriptableObject>(@"Dialogues\dialogue1");
+        }
+
+        public void HandleGetNextLineButtonPressed()
+        {
+
+            if (textWriterSingle != null && textWriterSingle.IsActive())
+            {
+                // Currently active TextWriter
+                textWriterSingle.WriteAllAndDestroy();
+            }
+            else
+            {
+                string message = _dialogue.GetNextLine();
+                if (message != null)
+                {
+                    textWriterSingle = TextWriter.AddWriter_Static(uiText: text, textToWrite: message,
+                        timePerCharacter: .01f, invisibleCharacters: true, removeWriterBeforeAdd: false, onComplete: null);
+                }
+            }
+        }
+
+
+
+    }
+}
